1d9582467459c846d8a1d5aa4da3ce4ca9ff9fcd
Dat Tran <datitran@gmail.com>
2022-10-26 11:04:46 +0200

    chore: add generatemockdata

---
+++ generatemockdata/function.py
@@ -0,0 +1,23 @@
+from faker import Faker
+
+fake = Faker()
+
+person = ["first_name", "first_name_female", "first_name_male", "last_name", "last_name_female", "last_name_male", "name"]
+address = ["address", "building_number" "city", "city_suffix", "country", "country_code", "postcode", "street_address", "street_name"]
+company = ["company"]
+job = ["job"]
+phone_number = ["phone_number"]
+internet = ["company_email", "email", "free_email"]
+
+faker_args = sum([person, address, company, job, phone_number, internet], [])
+
+def return_fake_name(row, faker_provider, valid_args=faker_args):
+  if faker_provider in valid_args:
+    return eval(f"fake.{faker_provider}()")
+  else:
+    return fake.name()
+
+def handler(event, context):
+  arg_two = list(map(lambda row: row[1], event["BatchedArgs"]))[0]
+  arg_one = list(map(lambda row: return_fake_name(row[0],  arg_two), event["BatchedArgs"]))
+  return arg_one
