31ec5ce0865cdbccf8f2dcd2e5e15c19e2a33163
Dat Tran <datitran@gmail.com>
2022-11-10 17:32:49 +0100

    chore: return None instead of -1

--- priceloopcurrency/function.py
+++ priceloopcurrency/function.py
@@ -5,7 +5,7 @@ def return_currency_rates(from_currency: str, to_currency: str) -> float:
   try:
     return str(c.get_rate(from_currency, to_currency))
   except:
-    return str(-1)
+    return None
 
 def handler(event, context):
   return list(map(lambda row: return_currency_rates(row[0], row[1]), event["BatchedArgs"]))
